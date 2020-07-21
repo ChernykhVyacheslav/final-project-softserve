2ab5b83334328fcdb5ff1b1ba8c1a13e76bf543e
Chupyra Serhii <ch.sergijgmail.com>
2020-07-22 00:58:17 +0300

    SprintScore class constructor & getters

--- src/main/java/com/softserve/edu/dto/SprintScore.java
+++ src/main/java/com/softserve/edu/dto/SprintScore.java
@@ -3,5 +3,17 @@ package com.softserve.edu.dto;
 public class SprintScore {
     private String sprintName;
     private int score;
-    // TODO
+
+    public SprintScore(String sprintName, int score) {
+        this.sprintName = sprintName;
+        this.score = score;
+    }
+
+    public String getSprintName() {
+        return sprintName;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
