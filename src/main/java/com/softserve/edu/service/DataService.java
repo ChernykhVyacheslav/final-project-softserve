210f0e6916c96f9dae19cffde9ffbedef83f92d2
Chupyra Serhii <ch.sergijgmail.com>
2020-07-22 00:47:11 +0300

    getStudents & getMentors

--- src/main/java/com/softserve/edu/service/DataService.java
+++ src/main/java/com/softserve/edu/service/DataService.java
@@ -1,5 +1,9 @@
 package com.softserve.edu.service;
 
+import com.softserve.edu.entity.Entity;
+
+import java.util.List;
+
 public interface DataService {
 
     public void addStudent(String studentName);
@@ -11,5 +15,8 @@ public interface DataService {
     public void addCommunication(String studentName, String mentorName);
     
     public void addSolution(String studentName, String sprintName, int score);
-    
+
+    public List<Entity> getStudents();
+
+    public List<Entity> getMentors();
 }
