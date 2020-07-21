622a432758ff7ba4ceef7d025097b3c86f135edb
ChernykhVyacheslav <chernihvyacheslav@gmail.com>
2020-07-21 22:40:51 +0300

    Constructor and getters/setters for Solution class.

--- src/main/java/com/softserve/edu/entity/Solution.java
+++ src/main/java/com/softserve/edu/entity/Solution.java
@@ -4,5 +4,26 @@ public class Solution {
     private int idStudent;
     private int idSprint;
     private int score;
-    // TODO
+
+    public Solution(int idStudent, int idSprint, int score) {
+        this.idStudent = idStudent;
+        this.idSprint = idSprint;
+        this.score = score;
+    }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public int getIdSprint() {
+        return idSprint;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 }
