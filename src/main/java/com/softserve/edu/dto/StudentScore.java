cbd90ca33c173232e4a1aee0f3ac8083d73d3b89
Chupyra Serhii <ch.sergijgmail.com>
2020-07-22 01:37:42 +0300

    StudentScore constructor, getters

--- src/main/java/com/softserve/edu/dto/StudentScore.java
+++ src/main/java/com/softserve/edu/dto/StudentScore.java
@@ -1,9 +1,26 @@
 package com.softserve.edu.dto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentScore {
     private String studentName;
     private List<SprintScore> sprintScore;
-    // TODO
+
+    public StudentScore(String studentName) {
+        this.studentName = studentName;
+        this.sprintScore = new ArrayList<SprintScore>();
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public List<SprintScore> getSprintScore() {
+        return sprintScore;
+    }
+
+    public void addSprintScore(SprintScore newSprintScore){
+        sprintScore.add(newSprintScore);
+    }
 }
