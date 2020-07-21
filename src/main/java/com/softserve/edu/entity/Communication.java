895f139516dc6cfafcad35c5301fea4fe279dc1e
Chupyra Serhii <ch.sergijgmail.com>
2020-07-21 21:10:19 +0300

    constructor and getters/setters for class Communication

--- src/main/java/com/softserve/edu/entity/Communication.java
+++ src/main/java/com/softserve/edu/entity/Communication.java
@@ -3,5 +3,25 @@ package com.softserve.edu.entity;
 public class Communication {
     private int idStudent;
     private int idMentor;
-    // TODO
+
+    public Communication(int idStudent, int idMentor) {
+        this.idStudent = idStudent;
+        this.idMentor = idMentor;
+    }
+
+    public int getIdStudent() {
+        return idStudent;
+    }
+
+    public void setIdStudent(int idStudent) {
+        this.idStudent = idStudent;
+    }
+
+    public int getIdMentor() {
+        return idMentor;
+    }
+
+    public void setIdMentor(int idMentor) {
+        this.idMentor = idMentor;
+    }
 }
